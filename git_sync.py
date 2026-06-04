24542568e0167786da6479a0e1f1671a784b99d5
lebastiansiehl-collab <lebastian.siehl@gmail.com>
2026-06-04 10:55:21 +0200

    Update git_sync.py

--- git_sync.py
+++ git_sync.py
@@ -1,9 +1,13 @@
-import git
 import os
+from git import Repo
 
 def sync_db():
-    repo = git.Repo('.')
+    token = os.environ.get("GIT_TOKEN")
+    repo = Repo('.')
+    # Nutze das Token für den Remote-Zugriff
     repo.git.add('data/arbeitsmedizin.db')
-    repo.git.commit('-m', 'Automatisches Datenbank-Backup')
+    repo.git.commit('-m', 'Auto-Sync via App')
+    # Hier setzen wir die URL mit Token zusammen
     origin = repo.remote(name='origin')
+    origin.set_url(f"https://{token}@github.com/lebastiansiehl-collab/company.git")
     origin.push()
