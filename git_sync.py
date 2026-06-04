da6b39387f6b71522edcc07ff9e60b8079202740
lebastiansiehl-collab <lebastian.siehl@gmail.com>
2026-06-04 12:39:30 +0200

    Update git_sync.py

--- git_sync.py
+++ git_sync.py
@@ -1,13 +1,18 @@
+import streamlit as st
 import os
 from git import Repo
 
 def sync_db():
-    token = os.environ.get("GIT_TOKEN")
+    token = st.secrets["GIT_TOKEN"]
     repo = Repo('.')
-    # Nutze das Token für den Remote-Zugriff
+    
+    # WICHTIG: Identität für Git setzen
+    repo.config_writer().set_value("user", "name", "StreamlitApp").release()
+    repo.config_writer().set_value("user", "email", "app@streamlit.com").release()
+    
     repo.git.add('data/arbeitsmedizin.db')
     repo.git.commit('-m', 'Auto-Sync via App')
-    # Hier setzen wir die URL mit Token zusammen
+    
     origin = repo.remote(name='origin')
     origin.set_url(f"https://{token}@github.com/lebastiansiehl-collab/company.git")
     origin.push()
